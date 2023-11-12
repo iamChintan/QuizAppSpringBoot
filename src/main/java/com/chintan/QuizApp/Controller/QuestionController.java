57a5fcc725235f0bb7c44ef782ab89257d368082
iamChintan <88285492+iamChintan@users.noreply.github.com>
2023-11-12 09:45:38 -0500

    WithStatusCode

--- src/main/java/com/chintan/QuizApp/Controller/QuestionController.java
+++ src/main/java/com/chintan/QuizApp/Controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.chintan.QuizApp.Controller;
 import com.chintan.QuizApp.Model.Question;
 import com.chintan.QuizApp.Service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,17 +15,17 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("allquestions")
-    public List<Question> getAllQuestion(){
+    public ResponseEntity<List<Question>> getAllQuestion(){
         return questionService.getAllQuestion();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category){
         return questionService.getQuestionByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return questionService.addQuestion(question);
     }
 }
--- src/main/java/com/chintan/QuizApp/Service/QuestionService.java
+++ src/main/java/com/chintan/QuizApp/Service/QuestionService.java
@@ -3,7 +3,11 @@ package com.chintan.QuizApp.Service;
 import com.chintan.QuizApp.Model.Question;
 import com.chintan.QuizApp.Repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,16 +16,31 @@ public class QuestionService {
 
     @Autowired
     QuestionRepository questionRepository;
-    public List<Question> getAllQuestion() {
-        return questionRepository.findAll();
+    public ResponseEntity<List<Question>> getAllQuestion() {
+        try {
+            return new ResponseEntity<>(questionRepository.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionByCategory(String category) {
-        return questionRepository.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionRepository.findByCategory(category), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return  new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public String addQuestion(Question question) {
-        questionRepository.save(question);
-        return "success";
+    public ResponseEntity<String> addQuestion(Question question) {
+        try{
+            questionRepository.save(question);
+            return new ResponseEntity<>("Success", HttpStatus.CREATED);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Failed", HttpStatus.BAD_REQUEST);
     }
 }
